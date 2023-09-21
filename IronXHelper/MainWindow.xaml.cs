5cf5055f592a9d3ffc67462259e5545c4a7e07d2
Kokon <prostonastya01@mail.ru>
2023-09-21 08:53:33 +0500

    add searching module by using exe pyhton app

--- IronXHelper/MainWindow.xaml.cs
+++ IronXHelper/MainWindow.xaml.cs
@@ -42,14 +42,24 @@ namespace IronXHelper
             }
         }
 
-        private async Task UpdateHelper()
+        private void BlockUI()
         {
             CheckUpdateButton.IsEnabled = FindHelpsButton.IsEnabled = false;
             Spinner.Visibility = Visibility.Visible;
             HelperInfoList = new List<HelperInfo>();
             OnPropertyChanged("HelperInfoList");
+            HelperList.SelectedItem = null;
             Thread.Sleep(10);
+        }
+        private void UnBlockUI()
+        {
+            Spinner.Visibility = Visibility.Collapsed;
+            CheckUpdateButton.IsEnabled = FindHelpsButton.IsEnabled = true;
+        }
 
+        private async Task UpdateHelper()
+        {
+            BlockUI();
             await Task<bool>.Run(async () =>
                await Downloader.DownloadInfo()).ContinueWith(t =>
             {
@@ -57,8 +67,7 @@ namespace IronXHelper
                 {
                     MessageBox.Show("Не удалось обновить информацию.");
                 }
-                Spinner.Visibility = Visibility.Collapsed;
-                CheckUpdateButton.IsEnabled = FindHelpsButton.IsEnabled = true;
+                UnBlockUI();
                 HelperInfoList = GetHelperInfoList();
                 OnPropertyChanged("HelperInfoList");
             }, TaskScheduler.FromCurrentSynchronizationContext());
@@ -72,6 +81,41 @@ namespace IronXHelper
         {
             FullScreenImage fullScreenImage = new FullScreenImage(((Image)sender).Source);
             fullScreenImage.ShowDialog();
+        }
+
+        private async Task Searching(string searchLine)
+        {
+            BlockUI();
+            await Task<bool>.Run(async () =>
+               await SearchingHelperInfo.Searching(searchLine)).ContinueWith(t =>
+               {
+                   UnBlockUI();
+                   HelperInfoList = t.Result;
+                   OnPropertyChanged("HelperInfoList");
+               }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+        private async void FindHelpsButton_Click(object sender, RoutedEventArgs e)
+        {
+            BlockUI();
+            if (SearchPanelBox.Text == "")
+            {
+                HelperInfoList = GetHelperInfoList();
+                OnPropertyChanged("HelperInfoList");
+                UnBlockUI();
+            }
+            else
+            {
+                Searching(SearchPanelBox.Text);
+            }
+        }
+
+        private void SearchPanelBox_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Enter)
+            {
+                FindHelpsButton_Click(null, null);
+            }
         }
     }
 }
---
+++ IronXHelper/Searching.cs
@@ -0,0 +1,48 @@
+﻿using Library.Functions;
+using Library.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace IronXHelper
+{
+    public static class SearchingHelperInfo
+    {
+        public static Task<List<HelperInfo>> Searching(string searchLine)
+        {
+            var data = JsonDocument.Parse(BaseFunctions.GetStringFileFromResources("DBConfig.json"));
+            string dbPath = Path.Combine(BaseFunctions.GetDocumentFolder(), data.RootElement.GetProperty("DBName").ToString());
+
+            string idString = "";
+            using (System.Diagnostics.Process p = new System.Diagnostics.Process())
+            {
+                p.StartInfo = new System.Diagnostics.ProcessStartInfo("searchingModule.exe", $"-DBpath=\"{dbPath}\" -query=\"{searchLine}\"");
+                p.StartInfo.CreateNoWindow = true;
+                p.StartInfo.ErrorDialog = false;
+                p.StartInfo.RedirectStandardError = true;
+                p.StartInfo.RedirectStandardInput = true;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                p.Start();
+
+                p.WaitForExit();
+                idString = p.StandardOutput.ReadToEnd();
+                p.Kill();
+            }
+
+            IronContext context = new IronContext();
+            idString = Regex.Replace(idString, "[^\\d ]+", "");
+            var ids = idString.Split(" ");
+            List<HelperInfo> infos = new List<HelperInfo>();
+            foreach (string id in ids)
+            {
+                infos.Add(context.HelperInfo.Where(x => x.Id.ToString() == id).First());
+            }
+            return Task.Run(() => infos); 
+        }
+    }
+}
